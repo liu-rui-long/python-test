a5c2873b96065c24fbfb0cf094d3db2fcd65f60a
liu-rui-long <long78526@163.com>
2025-12-30 09:40:48 +0800

    def

--- pyp.py
+++ pyp.py
@@ -1,7 +1,17 @@
-
+import math
 def my_abs(x):
     if x>=0:
         return x
     else:
         return -x
 print(my_abs(-6))
+
+def quadratic(a,b,c):
+    delta=b**2-4*a*c
+    if delta<0:
+        return None
+    else:
+        x1=(-b+math.sqrt(delta))/(2*a)
+        x2=(-b-math.sqrt(delta))/(2*a)
+        return x1,x2
+print(quadratic(2,3,1))
